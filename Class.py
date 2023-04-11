dfd852bcf76d81e6735aafe3611e0fe8dbb1e289
fabien <dev.fabien99@gmail.com>
2023-04-11 14:09:04 +0000

    add contact, receive sms after registration

--- Class.py
+++ Class.py
@@ -2,6 +2,8 @@ import uuid
 import hashlib
 import random
 import string
+from flask import jsonify
+from sendSMS import sendSMS
 
 def generate_unique_code():
     # generate a random 4-digit number
@@ -33,20 +35,27 @@ class User:
                     sql = "INSERT INTO users (public_id, fullname, phone, password, code) VALUES (%s, %s, %s, %s, %s)"
                     cursor.execute(sql, (public_id, fullname, phone, hashed_password, code))
                     self.db.commit()
-                    return dict(success=True, msg="Inserted ok!", data=[])
+                    sendSMS("AAMI", phone, f"you activation code is {code}")
+                    return jsonify(dict(success=True, msg="Inserted ok! Activate your account on here <a href='/confirm'>activate</a>", data=[]))
             except Exception as e:
-                return dict(success=False, msg=e, data=[])
+                return jsonify(dict(success=False, msg=e, data=[]))
             finally:
                 self.db.close()
-        return dict(success=False, msg="Phone already exist", data=[])
+        return jsonify(dict(success=False, msg="Phone already exist", data=[]))
             
     def login(self, phone, password):
         hashed_password = hashlib.sha256(password.encode()).hexdigest()
-        user = self.select_by_phone(phone)
-        if user and user['password'] == hashed_password:
-            return dict(success=True, msg="", data=user)
-        else:
-            return dict(success=False, msg="Invalid credentials!", data=[])
+        try:
+            with self.db.cursor() as cursor:
+                sql = "SELECT * FROM users WHERE phone = %s AND password = %s AND active=1"
+                cursor.execute(sql, (phone,hashed_password))
+                result = cursor.fetchone()
+                if result:
+                    return dict(success=True, msg="Login", data=result)
+                else:
+                    return dict(success=False, msg="Invalid credentials", data=[])
+        except Exception as e:
+            return dict(success=False, msg=e, data=[])
             
     def select_by_phone(self, phone):
         with self.db.cursor() as cursor:
@@ -54,12 +63,7 @@ class User:
             cursor.execute(sql, (phone,))
             result = cursor.fetchone()
             if result:
-                return {
-                    'id': result['public_id'],
-                    'fullname': result['fullname'],
-                    'phone': result['phone'],
-                    'password': result['password']
-                }
+                return result
             else:
                 return False
             
@@ -69,22 +73,55 @@ class User:
             cursor.execute(sql, (public_id,))
             result = cursor.fetchone()
             if result:
-                return {
-                    'id': result['public_id'],
-                    'fullname': result['fullname'],
-                    'phone': result['phone'],
-                    'password': result['password']
-                }
+                return result
+            else:
+                return False
+            
+    def select_by_id(self, id):
+        with self.db.cursor() as cursor:
+            sql = "SELECT * FROM users WHERE id = %s"
+            cursor.execute(sql, (id,))
+            result = cursor.fetchone()
+            if result:
+                return result
             else:
                 return False
             
     def activate(self, phone, code):
         try:
             with self.db.cursor() as cursor:
-                cursor.execute("UPDATE users SET active = 1 WHERE phone = %s AND code = %s", (phone,code))
-                self.db.commit()
-            return dict(success=True, msg="Account activate, your can login now", data=[])
+                sql = "SELECT * FROM users WHERE phone = %s AND code = %s"
+                cursor.execute(sql, (phone,code))
+                result = cursor.fetchone()
+                if result:
+                    cursor.execute("UPDATE users SET active = 1 WHERE phone = %s AND code = %s", (phone,code))
+                    self.db.commit()
+                    return dict(success=True, msg="Account activate, your can login now", data=[])
+                return dict(success=False, msg="Account activate, your can login now", data=[])
         except Exception as e:
             return dict(success=False, msg=e, data=[])
-        finally:
-            self.db.close()
+        
+    def addcontact(self, public_id, fullname, phone):
+        user = self.select_by_public_id(public_id)
+        if user:
+            try:
+                with self.db.cursor() as cursor:
+                    sql = "INSERT INTO contacts (fullname, phone, user_id) VALUES (%s, %s, %s)"
+                    cursor.execute(sql, (fullname, phone, user['id']))
+                    self.db.commit()
+                    return jsonify(dict(success=True, msg="Contact added!", data=[]))
+            except Exception as e:
+                return jsonify(dict(success=False, msg=e, data=[]))
+        return jsonify(dict(success=False, msg="User doesn't exist", data=[]))
+    
+    def get_contact(self, public_id):
+        user=self.select_by_public_id(public_id)
+        if user:
+            with self.db.cursor() as cursor:
+                sql = "SELECT * FROM contacts WHERE user_id = %s"
+                cursor.execute(sql, (user['id'],))
+                result = cursor.fetchall()
+                if result:
+                    return result
+                else:
+                    return []
--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, url_for, redirect, request, jsonify, session
 import pymysql
 from Class import *
+import re
 
+#sendSMS()
 db = pymysql.connect(
     host='localhost',
     user='root',
@@ -16,7 +18,7 @@ app=Flask(__name__)
 app.secret_key = 'my_secret_key'
 
 user = User(db)
-
+#send_email("fabienbrou99@gmail.com", "test", "Hello")
 
 #print(user.register("Fabien Brou", "(225)0153148864", "fabienbrou"))
 
@@ -32,16 +34,49 @@ def home():
 
 @app.route('/register', methods=['POST','GET'])
 def register():
-    error = []
+    errors = []
+    
+    # phone pattern +2250101010101
+    phone_pattern = r"\+\d{1,3}\d{9,10}"
+    password_pattern = r"^[A-Za-z0-9]{8,}$"
+
+    
     if request.method == 'POST':
-        pass
+        posted = request.form
+        
+        fullname = posted.get('fullname',"").strip().lower()
+        phone = posted.get('phone',"").strip().lower()
+        password = posted.get('password',"")
+        
+        if len(fullname) < 4 or len(fullname) > 50:
+            errors.append("Fullname must be between 4 and 50 characteres")
+        
+        match = re.match(phone_pattern, phone)
+        if not match:
+            errors.append("Phone don't respect the pattern (+/countryCode/numberPhone)")
+        
+        match = re.match(password_pattern, password)
+        if not match:
+            errors.append("Password must contains at least 8 characteres")
+            
+        if errors:
+            return jsonify(dict(success=False, msg=errors))
+        
+        return user.register(fullname,phone,password)
+        
     return render_template('register.html')
 
 @app.route('/confirm', methods=['POST','GET'])
 def confirm():
     error = []
     if request.method == 'POST':
-        pass
+        posted = request.form
+        
+        result = user.activate(posted['phone'], posted['code'])
+        if result['success']:
+            return jsonify(dict(success=True, msg="Account activated, you can login now <a href='/login'>here</a>"))
+        else :
+            return jsonify(dict(success=False, msg="code or phone is wrong"))
     return render_template('confirm.html')
 
 @app.route('/login', methods=['POST','GET'])
@@ -52,9 +87,10 @@ def login():
     
         result = user.login(posted['phone'], posted['password'])
         if result['success']:
-            session['AAMI_ID'] = result['data']['id']
+            session['AAMI_ID'] = result['data']['public_id']
             return redirect(url_for('dashboard'))
-        print(result)
+        else :
+            return result
     return render_template('login.html')
 
 @app.route('/dashboard')
@@ -62,7 +98,7 @@ def dashboard():
     if not user.select_by_public_id(session.get('AAMI_ID')):
             return redirect(url_for('home'))
 
-    return render_template('customers/index.html')
+    return render_template('customers/index.html', user=user.select_by_public_id(session.get('AAMI_ID')))
 
 @app.route('/parameters', methods=['POST','GET'])
 def parameters():
@@ -86,7 +122,7 @@ def contacts():
     if not user.select_by_public_id(session.get('AAMI_ID')):
             return redirect(url_for('home'))
 
-    return render_template('customers/contacts.html')
+    return render_template('customers/contacts.html', contacts=user.get_contact(session.get('AAMI_ID')))
 
 @app.route('/newcontact', methods=['GET', 'POST'])
 def addcontact():
@@ -95,7 +131,9 @@ def addcontact():
 
     error = []
     if request.method == 'POST':
-        pass
+        posted = request.form
+        
+        return user.addcontact(session.get('AAMI_ID'), posted['fullname'], posted['phone'])
     return render_template('customers/addcontact.html')
 
 @app.route('/calls')
